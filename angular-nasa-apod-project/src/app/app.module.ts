a621b07a32a18c41efc9c4515dc56622e3050ddd
Stacey Lawlor <stacey.tlawlor@outlook.com>
2023-05-23 14:41:06 +0100

    Add home page component

--- angular-nasa-apod-project/src/app/app.module.ts
+++ angular-nasa-apod-project/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NavComponent } from './components/nav/nav.component';
 import { PicOfDayComponent } from './components/pic-of-day/pic-of-day.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { DatePickerComponent } from './components/date-picker/date-picker.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { DatePickerComponent } from './components/date-picker/date-picker.compon
     NavComponent,
     PicOfDayComponent,
     FooterComponent,
-    DatePickerComponent
+    DatePickerComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- angular-nasa-apod-project/src/app/components/date-picker/date-picker.component.ts
+++ angular-nasa-apod-project/src/app/components/date-picker/date-picker.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./date-picker.component.scss']
 })
 export class DatePickerComponent {
-
+  public selectedDate: string | undefined;
 }
