1d3fe2654ed0c3800cde144f0a55207ca6c9a3d4
Stacey Lawlor <stacey.tlawlor@outlook.com>
2023-05-31 16:37:14 +0100

    Implement max date input

--- angular-nasa-apod-project/src/app/components/date-picker/date-picker.component.ts
+++ angular-nasa-apod-project/src/app/components/date-picker/date-picker.component.ts
@@ -1,7 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { NgbDate, NgbDateStruct  } from '@ng-bootstrap/ng-bootstrap';
 
-
 @Component({
   selector: 'app-date-picker',
   templateUrl: './date-picker.component.html',
@@ -9,7 +8,12 @@ import { NgbDate, NgbDateStruct  } from '@ng-bootstrap/ng-bootstrap';
 })
 export class DatePickerComponent {
   @Output() public dateSelected = new EventEmitter<any>();
-  public minDate: NgbDateStruct = { day: 16, month: 6, year: 1995 }
+  public minDate: NgbDateStruct = { day: 16, month: 6, year: 1995 };
+  public maxDate: NgbDateStruct = {
+    day: new Date().getDate(),
+    month: new Date().getMonth() + 1,
+    year: new Date().getFullYear()
+  }
 
   public onDateSelect(date: NgbDate): void {
     this.dateSelected.emit(date);
